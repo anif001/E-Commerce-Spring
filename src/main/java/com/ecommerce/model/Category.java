6bfc2a14faeece66deaaa153a612bcf03c762226
anif001 <shaikanif1190@gmail.com>
2026-06-14 19:12:35 +0530

    Add category entity

---
+++ src/main/java/com/ecommerce/model/Category.java
@@ -0,0 +1,34 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Category> subCategories = new ArrayList<>();
+
+    public void addSubCategory(Category subCategory) {
+        subCategories.add(subCategory);
+        subCategory.setParent(this);
+    }
+}
